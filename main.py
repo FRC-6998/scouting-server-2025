067c320174c4ab511fc9c29ba891d0baa03b2568
Shawn Lin <spig100.roc@gmail.com>
2025-01-24 13:22:03 +0800

    Adopt MongoDB client in this project.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
+# MongoDB connection setting
+MONGO_URI = "mongodb://localhost:27017"
+DATABASE_NAME = "scouting-field"
+
+# Generate MongoDB client
+client = AsyncIOMotorClient(MONGO_URI)
+db = client[DATABASE_NAME]
 
 @app.get("/")
 async def root():
