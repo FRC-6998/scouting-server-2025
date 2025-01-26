eb09d751c084491c879722a25a39cdb10f537bce
Shawn Lin <spig100.roc@gmail.com>
2025-01-27 01:31:01 +0800

    Modify mode.py to match the requirement according to the 1st scouting subteam meeting.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 from enum import Enum
 from fastapi import FastAPI, responses, HTTPException, status
-from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.params import Body
+from pymongo import MongoClient, AsyncMongoClient
+
 from model import *
 from pydantic import BaseModel
+import pymongo
 
 scouting_app = FastAPI()
 
@@ -16,8 +18,9 @@ MONGO_URI = "mongodb://localhost:27017"
 DATABASE_NAME = "scouting-field"
 
 # Generate MongoDB client
-client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncMongoClient("localhost", 27017)
 db = client[DATABASE_NAME]
+raw_collection = db["raw_data"]
 
 # OBJECTIVE MATCH DATA
 # Utilities
@@ -31,36 +34,3 @@ def delete_match_data(finding_uuid: UUID):
     for match in match_data_temp:
         if match.match_uuid == finding_uuid:
             match_data_temp.remove(match)
-
-# Temp file modification
-
-@scouting_app.post("/postObjectiveMatchTempData")
-async def post_objective_match_temp_data(obj_data: ObjectiveMatchData):
-    match_data_temp.append(obj_data)
-    return {
-        "message":
-            "Match data has saved to temporary list. Please verify it and save it to the database later."
-    }
-
-@scouting_app.get("/getObjectiveMatchTempData")
-async def get_objective_match_temp_data():
-    return match_data_temp
-
-@scouting_app.get("/getObjectiveMatchTempData/{match_id}")
-async def get_objective_match_temp_data_by_uuid(match_id: UUID):
-    result = find_match_data(match_id)
-    return result
-
-@scouting_app.delete("/deleteObjectiveMatchTempData/{match_id}")
-async def delete_objective_match_temp_data_by_uuid(match_id: UUID):
-    delete_match_data(match_id)
-
-# Save data to the db
-# @scouting_app.get("/saveObjectiveMatchData")
-# async def save_objective_match_data():
-#     for match in match_data_temp:
-#         await db.objective_match_data.insert_one(match.dict())
-#     return {
-#         "message":
-#             "All match data has been saved to the database. You can now delete the temporary list."
-#     }
