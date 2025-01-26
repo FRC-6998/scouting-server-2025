15ca7d6314bbcab2695b556c15cf324063bfee78
Shawn Lin <spig100.roc@gmail.com>
2025-01-27 01:31:01 +0800

    Modify model.py to match the requirement according to the 1st scouting subteam meeting.

--- model.py
+++ model.py
@@ -1,28 +1,81 @@
-# TODO: Make sure all the parameters are suitable to the requirement driver teams provided.
-
 from uuid import uuid4
 from enum import Enum
-from uuid import UUID
+from typing import List
+
+from ulid import ULID
 
 from pydantic import BaseModel, PrivateAttr, computed_field
 
+# Objective Match Data (formerly known as Scouting Data)
 
-# Match type enum
+# Match basic information
+
 class MatchType(str, Enum):
     PRACTICE = "Practice"
     QUALIFICATION = "Qualification"
     PLAYOFF = "Playoff"
 
-# Model to verify input data
+class Alliance(str, Enum):
+    RED = "Red"
+    BLUE = "Blue"
+
+# Auto
+
+class Preload(str, Enum):
+    NONE = "None"
+    CORAL = "Coral"
+    ALGAE = "Algae"
+
+class AutoStartPosition(str, Enum):
+    SIDE = "Side"
+    CENTER = "Center"
+    MIDDLE = "Middle"
+
+class AutoPathPosition(str, Enum):
+    # TODO: List out all possible positions
+    EXAMPLE = "Example"
+
+class AutoPath(BaseModel):
+    second: float
+    position: str
+    success: bool = False
+
+class Auto(BaseModel):
+    preload: Preload = "None"
+    start_position: AutoStartPosition
+    leave: bool = False
+    auto_path: List[AutoPath]
+
+# Teleop
+
+class TeleopPath(BaseModel):
+    second: float
+    position: str
+    success: bool = False
+
+class Teleop(BaseModel):
+    teleop_path: List[TeleopPath]
+
+# Endgame
+
+class BargeAction(str, Enum):
+    NONE = "None"
+    PARK = "Park"
+    DEEP = "Deep"
+    SHALLOW = "Shallow"
+
+# Objective Match Data Model
+
 class ObjectiveMatchData(BaseModel):
-
     # "_id" field: Making sure that the data in temp and db has same id
-    _id: UUID = PrivateAttr(default_factory= lambda: uuid4())
-    @computed_field
-    @property
-    def data_id(self) -> UUID:
-        return self._id
-
+    _id: ULID
+    scout: str
     match_type: MatchType
     match_number: int
+    event_key: str
     team_number: int
+    alliance: Alliance
+    auto : Auto
+    teleop: Teleop
+    barge_action: BargeAction
+    barge_time: float
