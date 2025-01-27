f8a0316f363f84468d5700a88c69a3457f27c878
Shawn Lin <spig100.roc@gmail.com>
2025-01-27 12:26:33 +0800

    Adding SubjectiveMatchData.

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
-from uuid import uuid4
 from enum import Enum
 from typing import List
 
+from pydantic import BaseModel
 from ulid import ULID
 
-from pydantic import BaseModel, PrivateAttr, computed_field
 
 # Objective Match Data (formerly known as Scouting Data)
 
@@ -64,7 +63,7 @@ class BargeAction(str, Enum):
     DEEP = "Deep"
     SHALLOW = "Shallow"
 
-# Objective Match Data Model
+# General Model
 
 class ObjectiveMatchData(BaseModel):
     # "_id" field: Making sure that the data in temp and db has same id
@@ -79,3 +78,28 @@ class ObjectiveMatchData(BaseModel):
     teleop: Teleop
     barge_action: BargeAction
     barge_time: float
+
+# Subjective Match Data (formerly known as Super Scout Data)
+
+class SubjectiveRanking3(int, Enum):
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
+
+class SubjectiveRanking2(int, Enum):
+    FIRST = 1
+    SECOND = 2
+
+class SubjectiveMatchData(BaseModel):
+    _id: ULID
+    scout: str
+    match_type: MatchType
+    match_number: int
+    event_key: str
+    team_number: int
+    alliance: Alliance
+    driver_awareness: SubjectiveRanking3
+    coral_station_awareness: SubjectiveRanking2
+    num_score_on_net: int
+    mobility = SubjectiveRanking3
+    defense = SubjectiveRanking3
