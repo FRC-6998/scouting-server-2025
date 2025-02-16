ecd88756a9877628f46aa6a7d74eea0256b9f92a
Eran Hsu <eran0926@gmail.com>
2025-02-16 22:32:37 +0800

    fix: update MongoDB connection settings to use environment variables for host and port

--- backend/app/scripts/initdb.py
+++ backend/app/scripts/initdb.py
@@ -4,7 +4,8 @@ from ..constants import MONGO_URL, DATABASE_NAME, TEST_MONGO_URL
 
 
 def init_collection(collection_name: str):
-    client = AsyncMongoClient(TEST_MONGO_URL) # AsyncMongoClient(MONGO_URL)
+    print(f"Connecting to {MONGO_URL} and {DATABASE_NAME}")
+    client = AsyncMongoClient(MONGO_URL)
     db = client[DATABASE_NAME]
     collection = db[collection_name]
     return collection
