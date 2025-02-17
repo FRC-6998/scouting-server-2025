57f09cd232295f66667afda5d36033c974dbf54d
Shawn Lin <spig100.roc@gmail.com>
2025-02-17 23:29:36 +0800

    fix: Fixing calculating for cycle time
    issue #2

--- backend/app/routers/test.py
+++ backend/app/routers/test.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.app.scripts.objective_calculate import calc_reef_success_rate_by_side, count_processor_score
+from backend.app.scripts.objective_calculate import count_net_score, search_cycle_time, calc_cycle_time
 
 router = APIRouter(
     prefix= "/test"
@@ -10,6 +10,6 @@ reef_levels = ["l1", "l2", "l3", "l4"]
 
 
 @router.get("/")
-async def test(team_number: str = None):
-    print (await count_processor_score(team_number, "teleop"))
+async def test(team_number: str, cycle_type: str):
+    print (await calc_cycle_time(team_number, cycle_type))
     return {"message": "Tested successfully"}
