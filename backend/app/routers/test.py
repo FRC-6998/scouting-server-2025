63d8d36f1e4113841971b1ba0a73c5fd96d9ff80
Shawn Lin <spig100.roc@gmail.com>
2025-02-17 16:05:10 +0800

    test: Modified test routers

--- backend/app/routers/test.py
+++ backend/app/routers/test.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.app.scripts.objective_calculate import calc_reef_success_rate_by_side
+from backend.app.scripts.objective_calculate import calc_reef_success_rate_by_side, count_processor_score
 
 router = APIRouter(
     prefix= "/test"
@@ -10,6 +10,6 @@ reef_levels = ["l1", "l2", "l3", "l4"]
 
 
 @router.get("/")
-async def test(team_number: str, side: str):
-    print (await calc_reef_success_rate_by_side(team_number, side))
+async def test(team_number: str = None):
+    print (await count_processor_score(team_number, "teleop"))
     return {"message": "Tested successfully"}
