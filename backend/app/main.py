68a8006df2af5a3fba7456beb21aafb0d6ea8cbf
Shawn Lin <spig100.roc@gmail.com>
2025-02-22 14:10:55 +0800

    feat: Adding router to get team_number by querying match info
    issue #4

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from .routers import objective_scout, subjective_scout, pit_scout, test
+from .scripts.objective_calculate import raw_collection
 
 scouting_app = FastAPI(
     title="Scouting Field Server API",
@@ -12,3 +13,18 @@ scouting_app.include_router(objective_scout.router)
 scouting_app.include_router(subjective_scout.router)
 scouting_app.include_router(pit_scout.router)
 scouting_app.include_router(test.router)
+
+@scouting_app.get(
+    "/team_list",
+    name="Getting team list",
+    description="Getting team list from the database.",
+    response_description="Got team list successfully",
+)
+async def get_team_list(match_level: str, match_number: str):
+    dict_data = await raw_collection.find(
+        {"match_level": match_level, "match_number": match_number},
+        {"_id":0, "team_number":1}
+    ).to_list(None)
+    list_data = list(d["team_number"] for d in dict_data)
+    print (list_data)
+    return {"teams": list_data}
